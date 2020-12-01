6ee89e5b075e72f57a30e526c856a0c424aebc84
Chas <Hugginsc10@gmail.com>
2020-12-01 01:36:10 -0500

    working app

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -109,7 +109,6 @@ const SignUp = ({navigation}) => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: 'center',
         alignItems: "center"
     },
