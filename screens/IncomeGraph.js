bee99d5c09659f2abfc4e71757bdb719f4f237b3
lizzie friedman <alizafriedman123@gmail.com>
2021-01-12 13:21:06 -0500

    restructure data for income piechart

--- screens/IncomeGraph.js
+++ screens/IncomeGraph.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Dimensions, View, Text} from 'react-native';
 import {PieChart} from 'react-native-chart-kit';
-import Income from './Income';
-import { Dimensions, View } from "react-native";
-const screenWidth = Dimensions.get("window").width;
 
+
+
+const screenWidth = Dimensions.get('window').width;
 
 
 const IncomeGraph = ({incomes}) => {
@@ -19,55 +20,40 @@ const IncomeGraph = ({incomes}) => {
         useShadowColorFromDataset: false // optional
     };
 
-const data = [
-    {
-        // type: `${type}`,
-        type: 'type',
-        misc: 234,
-        color: '#661327',
-        legendFontColor: '#1E2923"',
-        legendFontSize: 15
-    },
-     {
-        type: 'type',
-        misc: 234,
-         color: '#B3576D',
-         legendFontColor: '#1E2923"',
+
+
+
+const data = incomes.map((income) => {
+    const apple = {
+        type: income.type,
+        amount: income.amount,
+        color: '#A95EC6',
+        legendFontColor: '#A95EC6',
         legendFontSize: 15
     }
-]
-
-
-    // const data = 
-    //     incomes.map((income) => {
-    //     type: `${type}`;
-    //     amount: `${amount}`;
-    //     color: '#B3576D';
-    //      legendFontColor: '#1E2923"';
-    //     legendFontSize: 15
-    //     })
+    
+    return apple;
     
 
-console.log(incomes)
+})
+
+console.log(data)
 return (
-
-<View>
+    <View>
         <PieChart
-            data={data}
-            width={screenWidth}
-            height={100}
-            chartConfig={chartConfig}
-            accessor={'misc'}
+        data={data}
+        width={screenWidth}
+        height={220}
+        chartConfig={chartConfig}
+        accessor={'amount'}
         backgroundColor={'transparent'}
-            paddingLeft={'15'}
-            center={[10,50]}
-            absolute
-        />
+        paddingLeft={'15'}
+        center={[10,50]}
+        absolute
+         />
     </View>
-
-    )
+)
 }
-
 
 
 export default IncomeGraph;
